102adf558a76a40b9e93622077a8d9e1d4412d14
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2025-06-11 22:54:52 -0400

    ignore read me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,11 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li className="mb-2 tracking-[-.01em]">
+            Build a portfolio sight
+          </li>
+          <li className="mb-2 tracking-[-.01em]">
+            Learn how to code with next.js
           </li>
         </ol>
 
@@ -39,7 +42,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Dan
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -47,7 +50,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our Dans
           </a>
         </div>
       </main>
